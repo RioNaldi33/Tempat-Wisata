f6b677467811e07ac48b0b00a78c9d98e73362df
Rionaldi33 <rionaldi259@gmail.com>
2024-06-16 22:41:01 +0700

    done

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,7 @@
-$(document).ready(function() {
-    $('#contactForm').on('submit', function(event) {
-        event.preventDefault();
-        var name = $('#name').val();
-        var email = $('#email').val();
-        var message = $('#message').val();
-
-        if (name && email && message) {
-            alert('Pesan Anda telah terkirim!');
-            $('#contactForm')[0].reset();
-        } else {
-            alert('Harap isi semua bidang!');
-        }
+$(document).ready(function () {
+    $('#contactForm').on('submit', function (e) {
+        e.preventDefault();
+        $('#confirmationMessage').removeClass('d-none');
+        this.reset();
     });
 });
